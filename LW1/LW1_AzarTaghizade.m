5c51a7f31da249a795aafcce12ecd368392b0842
azariento <azartaghizade@outlook.com>
2026-09-14 09:17:19 +0300

    Update LW1_AzarTaghizade.m

--- LW1/LW1_AzarTaghizade.m
+++ LW1/LW1_AzarTaghizade.m
@@ -12,3 +12,13 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]    |    F_2 [-x-]')
+
+% Using help-search methods
+help sin
+doc sin
+help title
+
+% Examining linspace, size and max
+doc linspace
+doc size
+doc max
