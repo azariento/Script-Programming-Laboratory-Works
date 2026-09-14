eb24542506445d7684d909f2e06eaff0d188e843
azariento <azartaghizade@outlook.com>
2026-09-14 09:16:49 +0300

    Verified that everything works

---
+++ LW1/LW1_AzarTaghizade.m
@@ -0,0 +1,14 @@
+% Azar Taghizade
+% EDIfu-25/2
+% 14.09.2026
+
+%% Mandatory Task
+
+% Example Code
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]    |    F_2 [-x-]')
